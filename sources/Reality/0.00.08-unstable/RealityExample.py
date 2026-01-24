e0ae8edc3173559a5666b6e56efb4e9d61cb7222
D/ffusloN <TsingXLingH@outlook.com>
2026-01-24 17:47:33 +0800

    Delete sources/Reality/0.00.08-unstable directory

--- sources/Reality/0.00.08-unstable/RealityExample.py
+++
@@ -1,9 +0,0 @@
-from interface import *
-from widgets import *
-
-def command() -> None: print('Clicked!')
-
-inter = Interface(interface_title = 'Hello, curel world.')
-button = Button(inter, 450, 10, 200, 30, 'Hello, world.', command = command)
-
-inter.runApplication()
