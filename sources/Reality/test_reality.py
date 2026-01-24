15cc4b1348ee60dde26300f59c549a1cfb7b8a25
D/ffusloN <TsingXLingH@outlook.com>
2026-01-24 18:07:05 +0800

    Implement tests for Reality module
    Add tests to validate Reality module functionality.

--- sources/Reality/test_reality.py
+++ sources/Reality/test_reality.py
@@ -1,3 +1,8 @@
-import Reality
+def test_reality():
+    import Reality
+    assert Reality.__version__, 'Fail'
 
-Reality.consoleLogout(log_String = 'Hello, Reality!')
+    assert Reality.widgets, 'Fail'
+    assert Reality.interface.Interface, 'Fail'
+    assert Reality.WidgetComposer, 'Fail'
+    assert Reality.WidgetHandler, 'Fail'
