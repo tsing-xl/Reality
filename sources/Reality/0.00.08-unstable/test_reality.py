f367c93f59a02f1968a140e783846410583931ba
D/ffusloN <TsingXLingH@outlook.com>
2026-01-24 17:38:50 +0800

    Update test_reality.py

--- sources/Reality/0.00.08-unstable/test_reality.py
+++ sources/Reality/0.00.08-unstable/test_reality.py
@@ -1,8 +1,9 @@
-from .interface import *
-from .widgets import *
+from interface import *
+from widgets import *
 
 _interface = Interface()
 
 test_button = Button(_interface)
 test_label = Label(_interface)
+
 test_checkbox = CheckBox(_interface)
