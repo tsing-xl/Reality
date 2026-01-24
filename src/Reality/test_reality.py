0ac111743859350e96fb2adf1b0dab2c15e7a174
D/ffusloN <TsingXLingH@outlook.com>
2026-01-24 17:38:33 +0800

    Delete src/Reality directory

--- src/Reality/test_reality.py
+++
@@ -1,8 +0,0 @@
-from .interface import *
-from .widgets import *
-
-_interface = Interface()
-
-test_button = Button(_interface)
-test_label = Label(_interface)
-test_checkbox = CheckBox(_interface)
