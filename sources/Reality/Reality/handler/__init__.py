4a6afe9f4b5796d8dff9072846af8b1b9aa0d043
D/ffusloN <TsingXLingH@outlook.com>
2026-03-11 21:54:37 +0800

    ref1

--- sources/Reality/Reality/handler/__init__.py
+++
@@ -1,8 +0,0 @@
-from .widgethandler import WidgetHandler
-
-__version__ = '1'
-__version_v2__ = (1, 'unstable')
-
-__all__: tuple[str] = (
-    'WidgetHandler', 
-)
--- sources/Reality/test_reality.py
+++
@@ -1,8 +0,0 @@
-def test_reality():
-    import Reality
-    assert Reality.__version__, 'Fail'
-
-    assert Reality.widgets, 'Fail'
-    assert Reality.interface.Interface, 'Fail'
-    assert Reality.WidgetComposer, 'Fail'
-    assert Reality.WidgetHandler, 'Fail'
